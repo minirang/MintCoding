ab5025df60c518127b558d97ca648ef7ee450d22
서울민트초코 <minseong130502@gmail.com>
2026-02-21 22:29:55 +0900

    Update app.js

--- js/app.js
+++ js/app.js
@@ -8,37 +8,11 @@ workspace = Blockly.inject("blocklyDiv", {
   zoom:{controls:true,wheel:true,startScale:1.0,maxScale:3,minScale:0.3,scaleSpeed:1.2},
   move:{scrollbars:true,drag:true,wheel:true}
 });
-
 }
+
 
 function runCode(){
   const code=Blockly.JavaScript.workspaceToCode(workspace);
   try{eval(code);}
   catch(e){alert("Error: "+e);}
-}
-
-function saveFile(){
-  const xml=Blockly.Xml.workspaceToDom(workspace);
-  const text=Blockly.Xml.domToText(xml);
-  const blob=new Blob([text]);
-  const a=document.createElement("a");
-  a.href=URL.createObjectURL(blob);
-  a.download="project.mnc";
-  a.click();
-}
-
-function loadFile(){
-  const input=document.createElement("input");
-  input.type="file";
-  input.onchange=e=>{
-    const file=e.target.files[0];
-    const reader=new FileReader();
-    reader.onload=()=>{
-      const xml=Blockly.Xml.textToDom(reader.result);
-      workspace.clear();
-      Blockly.Xml.domToWorkspace(xml,workspace);
-    };
-    reader.readAsText(file);
-  };
-  input.click();
 }
