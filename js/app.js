9487e772c13d9ac83e658163b05f29057c4f3d33
서울민트초코 <minseong130502@gmail.com>
2026-02-21 18:45:07 +0900

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,9 +1,15 @@
-const workspace = Blockly.inject("blocklyDiv", {
+let workspace;
+
+function initBlockly(){
+
+workspace = Blockly.inject("blocklyDiv", {
   toolbox: document.getElementById("toolbox"),
   grid:{spacing:20,length:3,colour:"#ccc",snap:true},
   zoom:{controls:true,wheel:true,startScale:1.0,maxScale:3,minScale:0.3,scaleSpeed:1.2},
   move:{scrollbars:true,drag:true,wheel:true}
 });
+
+}
 
 function runCode(){
   const code=Blockly.JavaScript.workspaceToCode(workspace);
