e22b2c103d1713e07e02ccf10bb98332e26c297f
서울민트초코 <minseong130502@gmail.com>
2026-02-22 22:31:57 +0900

    added Mint custom blocks

--- js/blocks.js
+++ js/blocks.js
@@ -7,12 +7,13 @@ Blockly.Blocks['mint_print'] = {
     this.setPreviousStatement(true, null);
     this.setNextStatement(true, null);
 
-    this.setColour(180);
+    this.setColour(120);
 
     this.setTooltip(TOOLTIP[lang].mint_print);
   }
 };
 
+////////////////////////////////////////////////////////////////////
 Blockly.Blocks['mint_wait'] = {
   init: function() {
 
@@ -22,17 +23,18 @@ Blockly.Blocks['mint_wait'] = {
 
     this.appendDummyInput()
         .appendField(DUMMYINPUT_APPENDFILED[lang].mint_wait);
-        
+
 
     this.setPreviousStatement(true, null);
     this.setNextStatement(true, null);
 
-    this.setColour(210);
+    this.setColour(120);
 
     this.setTooltip(TOOLTIP[lang].mint_wait);
   }
 };
 
+///////////////////////////////////////////////////////////////////
 Blockly.Blocks['mint_sha256'] = {
   init: function() {
 
@@ -42,8 +44,24 @@ Blockly.Blocks['mint_sha256'] = {
 
     this.setOutput(true, "String");
 
-    this.setColour(260);
+    this.setColour(120);
 
     this.setTooltip(TOOLTIP[lang].mint_sha256);
+  }
+};
+
+///////////////////////////////////////////////////////////////////////
+Blockly.Blocks['mint_trim'] = {
+  init: function() {
+
+    this.appendValueInput("TEXT")
+        .setCheck("String")
+        .appendField(APPENDFIELD[lang].mint_trim);
+
+    this.setOutput(true, "String");
+
+    this.setColour(120);
+
+    this.setTooltip(TOOLTIP[lang].mint_trim);
   }
 };
