28004dccef901716c6ffe3e729fe03196ef89e83
서울민트초코 <minseong130502@gmail.com>
2026-02-25 18:51:50 +0900

    Save and load added

--- js/app.js
+++ js/app.js
@@ -19,3 +19,52 @@ async function runCode(){
     alert("Error: "+e);
   }
 }
+
+function saveWorkspace() {
+  const data = Blockly.serialization.workspaces.save(workspace);
+  const json = JSON.stringify(data, null, 2);
+  const blob = new Blob([json], {type:"application/json"});
+  const a = document.createElement("a");
+  a.href = URL.createObjectURL(blob);
+  let name = document.getElementById("projectName").value.trim();
+  if (!name) {
+    name = "새 프로젝트";
+  }
+  a.download = name + ".mint";  a.click();
+  showToast("프로젝트가 저장되었습니다!", 2500);
+  console.log("저장 완료")
+}
+
+function loadWorkspace(event) {
+  const file = event.target.files[0];
+  if (!file) return;
+  const reader = new FileReader();
+  reader.onload = function(e) {
+    const data = JSON.parse(e.target.result);
+    workspace.clear();
+    Blockly.serialization.workspaces.load(data, workspace);
+  };
+  reader.readAsText(file);
+  document.getElementById("projectName").value =
+  file.name.replace(/\.mint$/i, "");
+  showToast("프로젝트가 로드되었습니다.", 2500);
+  document.title = `MintCoding Editor - ${document.getElementById("projectName").value.trim()}`;
+}
+
+// Ctrl + S 저장
+window.addEventListener('keydown', function (e) {
+  if ((e.ctrlKey || e.metaKey) && e.key === 's') {
+    e.preventDefault();
+    saveWorkspace();
+  }
+});
+
+function showToast(message, time) {
+  const toast = document.getElementById("toast");
+  toast.textContent = message;
+  toast.classList.add("show");
+
+  setTimeout(() => {
+    toast.classList.remove("show");
+  }, time);
+}
