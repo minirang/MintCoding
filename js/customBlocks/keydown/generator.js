479ed763803e9f7a2a74c3ea1ec8d788519b061f
서울민트초코 <minseong130502@gmail.com>
2026-03-01 09:00:57 +0900

    async 관련 버그수정

--- js/customBlocks/keydown/generator.js
+++ js/customBlocks/keydown/generator.js
@@ -3,20 +3,19 @@ javascriptGenerator.forBlock['keydown'] = function(block, generator) {
   const statements = generator.statementToCode(block, 'DO') || '';
   
   return `
-document.addEventListener('keydown', function(event) {
+document.addEventListener('keydown', async function(event) {
   if (event.key === "${key}") {
     ${statements}
   }
 });
 `;
-};
-////////////////////////////////////////////////////////////////////////
+};////////////////////////////////////////////////////////////////////////
 javascriptGenerator.forBlock['keyup'] = function(block, generator) {
   const key = block.getFieldValue('KEY');
   const statements = generator.statementToCode(block, 'DO') || '';
   
   return `
-document.addEventListener('keyup', function(event) {
+document.addEventListener('keyup', async function(event) {
   if (event.key === "${key}") {
     ${statements}
   }
